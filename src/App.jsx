78fedce98d8864695c71dd68fe4f7fabc0f5b749
cher lee <cherlee268@gmail.com>
2024-07-09 15:11:12 -0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,18 @@ function App() {
   
   return ( <div>
     
+   <h1>Top five shoes to buy!</h1>
+    <Image src="src\assets\shoesimage.jpg" width={"40%"} length={"20"} style={{aspectRatio: 1/1, width: "40%"}} ></Image>
+<p></p> These are the top five most stylish shoe brands to buy right now 
+
+<li></li> number 1: Nike 
+<ul></ul> Reasoning: Nike is a very popular brand founded in 1964 and is considered a very popular brand that has very stylish, reliable, comfortable, and durable shoes
+<ul></ul> number 2: addidas 
   </div>
+
   )
 }
 
 export default App
+
+
